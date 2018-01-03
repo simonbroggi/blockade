e5d3025977a0a2a6f17355413877a09e757c36e9
Simon Broggi <simon.broggi@insert-coin.ch>
2018-01-03 21:08:45 +0100

    no poll input system

--- game/input.lua
+++ game/input.lua
@@ -115,7 +115,7 @@ function Input.update(dt)
     -- poll the GPIO edge events
     if GPIO then
         for _, binding in pairs(Input.bindingsPerKey) do
-            if binding.gpio:poll(0) then
+            if binding.gpio:read() == false then
                 binding.pressed = true
             end
         end
